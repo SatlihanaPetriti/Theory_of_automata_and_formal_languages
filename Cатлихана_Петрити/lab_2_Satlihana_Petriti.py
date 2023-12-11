fa89f01169ade17377b90bdf931706adb965b4ff
s.p <101753975+SatlihanaPetriti@users.noreply.github.com>
2023-12-11 16:13:57 +0100

    -

--- Cатлихана_Петрити/lab_2_Satlihana_Petriti.py
+++ Cатлихана_Петрити/lab_2_Satlihana_Petriti.py
@@ -1,135 +1,155 @@
+import gym
 import numpy as np
-import matplotlib.pyplot as plt
 import pygame
-import random
+import sys
 import time
 
-# Настройка окружения
-WDW_SIZE = 10 # размер сетки (WDW_SIZE)
-NR_OBSTACLES = 3 # количество препятствий (NR_OBSTACLES) 
-START_POINT = (0, 0)# начальная точка (START_POINT)
-GOAL_POINT = (WDW_SIZE - 1, WDW_SIZE - 1) # конечная точка (GOAL_POINT)
- 
-# Параметры Q-обучения
-learning_rate = 0.1
-discount_factor = 0.9
-exploration_prob = 0.2
-episodes = 500
+# Определение класса CustomEnv, унаследованного от gym.Env
+class CustomEnv(gym.Env):
+    def __init__(self):
+        super(CustomEnv, self).__init__()
 
-# Инициализация Q-таблицы
-q_table = np.zeros((WDW_SIZE, WDW_SIZE, 4))  # 4 действия: вверх, вниз, влево, вправо
+        # Параметры среды
+        self.WDW_SIZE = 10  # Размер среды (10x10 сетка)
+        self.NR_OBSTACLES = 3  # Количество препятствий
+        self.START_POINT = (0, 0)  # Начальная точка агента
+        self.GOAL_POINT = (self.WDW_SIZE - 1, self.WDW_SIZE - 1)  # Целевая точка
 
-# Добавление препятствий в окружение
-obstacle_positions = [(2, 3), (5, 5), (8, 7)]
-for pos in obstacle_positions:
-    q_table[pos[0], pos[1], :] = -np.inf  # Сделать препятствия непроходимыми
+        # Определение пространства состояний и действий
+        self.observation_space = gym.spaces.Discrete(self.WDW_SIZE * self.WDW_SIZE)
+        self.action_space = gym.spaces.Discrete(4)
 
-# Константы для действий
-UP = 0
-DOWN = 1
-LEFT = 2
-RIGHT = 3
+        # Инициализация Q-таблицы
+        self.q_table = np.zeros((self.WDW_SIZE * self.WDW_SIZE, 4))
 
-# Инициализация Pygame
-pygame.init()
+        # Дополнительная инициализация при необходимости
+        self.state = None
+        self.agent_position = self.START_POINT
+        self.target_position = self.GOAL_POINT
 
-# Настройка окна
-WINDOW_SIZE = 400
-cell_size = WINDOW_SIZE // WDW_SIZE
-screen = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
-pygame.display.set_caption("Окружение для Q-обучения")
+        # Инициализация Pygame
+        pygame.init()
+        self.WINDOW_SIZE = 400  # Размер окна Pygame
+        self.cell_size = self.WINDOW_SIZE // self.WDW_SIZE  # Размер ячейки среды в окне
+        self.screen = pygame.display.set_mode((self.WINDOW_SIZE, self.WINDOW_SIZE))
+        pygame.display.set_caption("Q-обучение с OpenAI Gym")
 
-# Эта функция отвечает за рисование окружения. Она включает в себя сетку, препятствия, цель и текущее положение агента.
-def draw_environment(agent_pos):
-    screen.fill((255, 255, 255))  # Белый фон
+        # Позиции препятствий
+        self.obstacle_positions = [(2, 3), (5, 5), (8, 7)]
 
-    # Рисование сетки
-    for i in range(WDW_SIZE + 1):
-        pygame.draw.line(screen, (200, 200, 200), (0, i * cell_size), (WINDOW_SIZE, i * cell_size))
-        pygame.draw.line(screen, (200, 200, 200), (i * cell_size, 0), (i * cell_size, WINDOW_SIZE))
+    # Метод сброса среды в начальное состояние
+    def reset(self):
+        self.state = self.START_POINT[0] * self.WDW_SIZE + self.START_POINT[1]
+        self.agent_position = self.START_POINT
+        return self.state
 
-    # Рисование препятствий
-    for pos in obstacle_positions:
-        pygame.draw.rect(screen, (255, 0, 0), (pos[1] * cell_size, pos[0] * cell_size, cell_size, cell_size))
+    # Метод выполнения одного шага в среде
+    def step(self, action):
+        current_position = (self.state // self.WDW_SIZE, self.state % self.WDW_SIZE)
 
-    # Рисование цели (цвет на желтый)
-    pygame.draw.circle(screen, (255, 255, 0),
-                       (GOAL_POINT[1] * cell_size + cell_size // 2, GOAL_POINT[0] * cell_size + cell_size // 2),
-                       cell_size // 2)
+        new_position = self.state_after_action(current_position, action)
 
-    # Рисование агента
-    pygame.draw.circle(screen, (0, 255, 0),
-                       (agent_pos[1] * cell_size + cell_size // 2, agent_pos[0] * cell_size + cell_size // 2),
-                       cell_size // 2)
+        done = (new_position == self.GOAL_POINT)
+        reward = -1 if not done else 10  # Награда за достижение цели (-1 за каждый шаг, 10 за достижение цели)
 
-    pygame.display.flip()
+        self.state = new_position[0] * self.WDW_SIZE + new_position[1] if new_position is not None else self.state
+        self.agent_position = new_position
 
-# Эта функция обрабатывает события Pygame. 
-# она проверяет наличие события QUIT (закрытие окна) и соответствующим образом завершает работу программы.
+        return self.state, reward, done, {}
+
+    # Метод визуализации среды
+    def render(self, mode='human'):
+        self.screen.fill((255, 255, 255))  # Белый фон
+
+        # Отрисовка сетки
+        for i in range(self.WDW_SIZE + 1):
+            pygame.draw.line(self.screen, (200, 200, 200), (0, i * self.cell_size), (self.WINDOW_SIZE, i * self.cell_size))
+            pygame.draw.line(self.screen, (200, 200, 200), (i * self.cell_size, 0), (i * self.cell_size, self.WINDOW_SIZE))
+
+        # Отрисовка препятствий
+        for pos in self.obstacle_positions:
+            pygame.draw.rect(self.screen, (255, 0, 0), (pos[1] * self.cell_size, pos[0] * self.cell_size, self.cell_size, self.cell_size))
+
+        # Отрисовка целевой точки
+        pygame.draw.circle(self.screen, (255, 255, 0),
+                           (self.GOAL_POINT[1] * self.cell_size + self.cell_size // 2,
+                            self.GOAL_POINT[0] * self.cell_size + self.cell_size // 2),
+                           self.cell_size // 2)
+
+        # Отрисовка текущей позиции агента
+        pygame.draw.circle(self.screen, (0, 255, 0),
+                           (self.agent_position[1] * self.cell_size + self.cell_size // 2,
+                            self.agent_position[0] * self.cell_size + self.cell_size // 2),
+                           self.cell_size // 2)
+
+        pygame.display.flip()
+
+    # Метод вычисления следующего состояния после выполнения действия
+    def state_after_action(self, state, action):
+        if action == 0 and state[0] > 0 and (state[0] - 1, state[1]) not in self.obstacle_positions:
+            return (state[0] - 1, state[1])
+        elif action == 1 and state[0] < self.WDW_SIZE - 1 and (state[0] + 1, state[1]) not in self.obstacle_positions:
+            return (state[0] + 1, state[1])
+        elif action == 2 and state[1] > 0 and (state[0], state[1] - 1) not in self.obstacle_positions:
+            return (state[0], state[1] - 1)
+        elif action == 3 and state[1] < self.WDW_SIZE - 1 and (state[0], state[1] + 1) not in self.obstacle_positions:
+            return (state[0], state[1] + 1)
+        else:
+            return state
+
+# Функция обработки событий Pygame
 def pygame_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            quit()
+            sys.exit()
 
-# Функция для ожидания некоторого времени
-def wait():
-    pygame.time.wait(100)
-
-# Функция для выбора действия на основе значения Q
-def choose_action(state):
-    if random.uniform(0, 1) < exploration_prob:
-        return random.choice([UP, DOWN, LEFT, RIGHT])
+# Функция выбора действия агента с учетом вероятности исследования
+def choose_action(state, exploration_prob):
+    if np.random.rand() < exploration_prob:
+        return np.random.choice([0, 1, 2, 3])  # Случайный выбор действия с вероятностью исследования
     else:
-        # Эта строка возвращает действие с наибольшим значением Q для данного состояния
-        return np.argmax(q_table[state[0], state[1], :])
+        return np.argmax(env.q_table[state, :])  # Выбор действия с наивысшим Q-значением для данного состояния
 
-# Эта функция реализует алгоритм Q-learning. 
-# Она выполняет итерацию по эпизодам, обновляя значения Q на основе взаимодействий агента с окружающей средой.
-def q_learning():
+# Функция Q-обучения
+def q_learning(env):
+    learning_rate = 0.1  # Коэффициент обучения
+    discount_factor = 0.9  # Коэффициент дисконтирования
+    initial_exploration_prob = 0.8  # Начальная вероятность исследования
+    episodes = 500  # Количество эпизодов
+
     for episode in range(episodes):
-        start_time = pygame.time.get_ticks()  # Запись времени начала эпизода
-        state = START_POINT
+        state = env.reset()  # Начальное состояние среды
+        exploration_prob = initial_exploration_prob / (episode + 1)  # Уменьшение вероятности исследования с течением времени
+
+        start_time = time.time()
+
         while True:
-            action = choose_action(state)
-            new_state = state_after_action(state, action)
-            reward = -1
+            action = choose_action(state, exploration_prob)
+            next_state, reward, done, _ = env.step(action)
 
-            if new_state == GOAL_POINT:
-                reward = 10
-                end_time = pygame.time.get_ticks()  # Запись времени окончания при достижении цели
-                time_taken = (end_time - start_time) / 1000.0  # Преобразование миллисекунд в секунды
-                print(f"Цель {episode + 1}: {time_taken} секунд")
-            #Эта строка обновляет Q-значение для текущей пары состояние-действие, используя формулу Q-learning
-            q_table[state[0], state[1], action] = (1 - learning_rate) * q_table[state[0], state[1], action] + \
-                                                  learning_rate * (
-                                                          reward + discount_factor * np.max(
-                                                      q_table[new_state[0], new_state[1], :]))
+            # Обновление Q-значения с использованием формулы Q-обучения
+            env.q_table[state, action] = (1 - learning_rate) * env.q_table[state, action] + \
+                                          learning_rate * (reward + discount_factor * np.max(env.q_table[next_state, :]))
 
-            state = new_state
-            draw_environment(state)
-            wait()
+            env.render()
             pygame_events()
+            pygame.time.wait(100)  # Пауза для визуализации
 
-            if state == GOAL_POINT:
+            state = next_state
+
+            if done:
+                end_time = time.time()
+                time_taken = end_time - start_time
+                print(f"Эпизод {episode + 1} | Время: {time_taken:.2f} секунды | Вероятность исследования: {exploration_prob:.2f}")
                 break
 
-# Функция для получения следующего состояния после выполнения действия
-def state_after_action(state, action):
-    if action == UP and state[0] > 0 and q_table[state[0] - 1, state[1], action] != -np.inf:
-        return (state[0] - 1, state[1])
-    elif action == DOWN and state[0] < WDW_SIZE - 1 and q_table[state[0] + 1, state[1], action] != -np.inf:
-        return (state[0] + 1, state[1])
-    elif action == LEFT and state[1] > 0 and q_table[state[0], state[1] - 1, action] != -np.inf:
-        return (state[0], state[1] - 1)
-    elif action == RIGHT and state[1] < WDW_SIZE - 1 and q_table[state[0], state[1] + 1, action] != -np.inf:
-        return (state[0], state[1] + 1)
-    else:
-        return state
-    # Эта строка возвращает новое состояние на основе действия. 
-    # Если действие допустимо, оно обновляет состояние; в противном случае оно остается в текущем состоянии.  
+# Создание экземпляра пользовательской среды
+env = CustomEnv()
 
-# Запуск алгоритма Q-обучения
-# Агент учится перемещаться от начальной точки к цели, избегая препятствий.
-q_learning()
+# Запуск Q-обучения
+q_learning(env)
 
+# Закрытие окна Pygame
+pygame.quit()
+sys.exit()
