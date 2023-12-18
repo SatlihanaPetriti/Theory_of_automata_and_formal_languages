a5f2eb594a0f8105f150b60190c850c902a423d8
s.p <101753975+SatlihanaPetriti@users.noreply.github.com>
2023-12-18 20:24:48 +0100

    ....

--- Cатлихана_Петрити/Lab1_Satlihana_P.py
+++ Cатлихана_Петрити/Lab1_Satlihana_P.py
@@ -1,56 +1,108 @@
-import random
+import gym  
+import pygame 
+import sys  
+import numpy as np  
 
-class Grid:
-    def __init__(self, size):
-        # Инициализация сетки заданного размера
-        self.size = size
-        self.grid10 = [['.' for _ in range(size)] for _ in range(size)]
+# Определение класса CustomEnv, наследующегося от класса gym.Env
+class CustomEnv(gym.Env):
+    def __init__(self):
+        super(CustomEnv, self).__init__()
 
-class Agent:
-    def __init__(self, grid10):
-        # Инициализация агента с указанием сетки
-        self.grid10 = grid10
-        # Установка случайной начальной позиции агента
-        self.row = random.randint(0, grid10.size - 1)
-        self.column = random.randint(0, grid10.size - 1)
+        # Параметры окружения
+        self.WDW_SIZE = 10
+        self.START_POINT = (0, 0)
 
-    def move(self):
-        # Выбор случайного направления движения
-        direction = random.choice(['up', 'down', 'left', 'right'])
+        # Определение пространства состояний и действий
+        self.observation_space = gym.spaces.Discrete(self.WDW_SIZE * self.WDW_SIZE)
+        self.action_space = gym.spaces.Discrete(4)
 
-        # Проверка и выполнение движения в выбранном направлении
-        if direction == 'up' and self.row > 0:
-            self.row -= 1
-        elif direction == 'down' and self.row < self.grid10.size - 1:
-            self.row += 1
-        elif direction == 'left' and self.column > 0:
-            self.column -= 1
-        elif direction == 'right' and self.column < self.grid10.size - 1:
-            self.column += 1
+        # Инициализация переменных состояния и позиции агента
+        self.state = None
+        self.agent_position = self.START_POINT
 
-# Создание сетки размера 10x10
-grid10 = Grid(10)
+        # Инициализация графического интерфейса с использованием библиотеки Pygame
+        pygame.init()
+        self.WINDOW_SIZE = 400
+        self.cell_size = self.WINDOW_SIZE // self.WDW_SIZE
+        self.screen = pygame.display.set_mode((self.WINDOW_SIZE, self.WINDOW_SIZE))
+        pygame.display.set_caption("Random Movement")
 
-# Инициализация агента с передачей ему сетки
-agent = Agent(grid10)
+    # Метод сброса среды в начальное состояние
+    def reset(self):
+        self.state = self.START_POINT[0] * self.WDW_SIZE + self.START_POINT[1]
+        self.agent_position = self.START_POINT
+        return self.state
 
-# Множество для отслеживания посещенных клеток
-cellvisit = set()
+    # Метод выполнения одного шага в среде
+    def step(self, action):
+        current_position = (self.state // self.WDW_SIZE, self.state % self.WDW_SIZE)
+        new_position = self.state_after_action(current_position, action)
 
-# Цикл, пока не посетят все клетки сетки
-while len(cellvisit) < grid10.size * grid10.size:
-    # Добавление текущей позиции агента в множество посещенных клеток
-    cellvisit.add((agent.row, agent.column))
-    # Перемещение агента
-    agent.move()
+        reward = -1
+        done = False
 
-# Вывод сетки с отметкой пути агента ('S'), посещенных клеток ('.'), и не посещенных клеток ('0')
-for row in range(grid10.size):
-    for column in range(grid10.size):
-        if (row, column) == (agent.row, agent.column):
-            print('S', end=' ')
-        elif (row, column) in cellvisit:
-            print('.', end=' ')
+        self.state = new_position[0] * self.WDW_SIZE + new_position[1] if new_position is not None else self.state
+        self.agent_position = new_position
+
+        if self.state == self.WDW_SIZE * self.WDW_SIZE - 1:
+            done = True
+
+        return self.state, reward, done, {}
+
+    # Метод отрисовки текущего состояния среды
+    def render(self, mode='human'):
+        self.screen.fill((255, 255, 255))
+
+        for i in range(self.WDW_SIZE + 1):
+            pygame.draw.line(self.screen, (200, 200, 200), (0, i * self.cell_size), (self.WINDOW_SIZE, i * self.cell_size))
+            pygame.draw.line(self.screen, (200, 200, 200), (i * self.cell_size, 0), (i * self.cell_size, self.WINDOW_SIZE))
+
+        pygame.draw.circle(self.screen, (0, 255, 0),
+                           (self.agent_position[1] * self.cell_size + self.cell_size // 2,
+                            self.agent_position[0] * self.cell_size + self.cell_size // 2),
+                           self.cell_size // 2)
+
+        pygame.display.flip()
+
+    # Метод для определения состояния после выполнения действия
+    def state_after_action(self, state, action):
+        if action == 0 and state[0] > 0:
+            return (state[0] - 1, state[1])
+        elif action == 1 and state[0] < self.WDW_SIZE - 1:
+            return (state[0] + 1, state[1])
+        elif action == 2 and state[1] > 0:
+            return (state[0], state[1] - 1)
+        elif action == 3 and state[1] < self.WDW_SIZE - 1:
+            return (state[0], state[1] + 1)
         else:
-            print('0', end=' ')
-    print()
+            return state
+
+# Функция для случайного перемещения агента в среде
+def random_movement(env):
+    episodes = 1
+
+    for episode in range(episodes):
+        state = env.reset()
+
+        while True:
+            action = np.random.choice([0, 1, 2, 3])  # Выбор случайного действия из пространства действий
+            next_state, reward, done, _ = env.step(action)  # Выполнение выбранного действия
+
+            env.render()  # Отрисовка текущего состояния среды
+            pygame.time.wait(100)  # Задержка для визуализации
+
+            state = next_state
+
+            if done:
+                print("Агент достиг каждой ячейки.")
+                break
+
+# Создание экземпляра среды
+env = CustomEnv()
+
+# Запуск случайного перемещения агента в среде
+random_movement(env)
+
+# Завершение работы Pygame и выход из программы
+pygame.quit()
+sys.exit()
